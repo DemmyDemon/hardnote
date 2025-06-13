6fc8fad77614bc40362422a452c1f5adf60aa7db
Fredrik Vold <demmydemon@gmail.com>
2025-06-13 16:02:40 +0200

    Vastly superior placeholder

--- ui/placeholder.go
+++ ui/placeholder.go
@@ -1,17 +1,35 @@
 package ui
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"strings"
 
-type Placeholder struct{}
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type Placeholder struct {
+	height int
+	width  int
+}
 
 func (ph Placeholder) Init() tea.Cmd {
 	return nil
 }
 
-func (ph Placeholder) Update(tea.Msg) (tea.Model, tea.Cmd) {
+func (ph Placeholder) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		ph.height = msg.Height - 1 // Leave room for status bar
+		ph.width = msg.Width
+	}
 	return ph, nil
 }
 
 func (ph Placeholder) View() string {
-	return "[PLACEHOLDER]"
+	title := "──┤ Placeholder ├"
+	line := strings.Repeat("─", max(0, ph.width-lipgloss.Width(title)))
+	view := lipgloss.JoinHorizontal(lipgloss.Center, title, line)
+	view += "\nThe view you are trying to use is not implemented yet."
+	view += strings.Repeat("\n", ph.height-2)
+	return view
 }
